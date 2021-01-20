1ef60619ad2fbf15ef7ba4472668e71b94e3046b
Jude Harding <jharding@genesisdla.com>
2021-01-20 17:23:27 -0500

    working thru vid 115

--- sect-09-cmp-adv-01-starting-setup/src/App.vue
+++ sect-09-cmp-adv-01-starting-setup/src/App.vue
@@ -1,48 +1,61 @@
 <template>
 	<div>
 		<the-header></the-header>
-		<badge-list></badge-list>
-		<user-info
-			:full-name="activeUser.name"
-			:info-text="activeUser.description"
-			:role="activeUser.role"
-		></user-info>
-		<course-goals>
-			<template #default="slotProps">
-				<h2>{{ slotProps.item }}</h2>
-				<p>
-					{{ slotProps["another-prop"] }}
-				</p>
-			</template>
-		</course-goals>
+		<button v-on:click="setSelectedComponent('active-goals')">
+			Active Goals
+		</button>
+		<button v-on:click="setSelectedComponent('manage-goals')">
+			Manage Goals
+		</button>
+
+		<!-- //regular components -->
+		<!-- <ActiveGoals v-if="selectedComponent === 'active-goals'"></ActiveGoals>
+		<ManageGoals v-if="selectedComponent === 'manage-goals'"></ManageGoals> -->
+
+		<!-- // dynamic components based on tab and keep alive holds data STATE -->
+		<keep-alive>
+			<component v-bind:is="selectedComponent"></component>
+		</keep-alive>
 	</div>
 </template>
 
 <script>
 // importing local components
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 // initializing local components
 export default {
 	components: {
 		// "the-header": TheHeader,
 		TheHeader,
-		BadgeList,
-		UserInfo,
-		CourseGoals,
+		// BadgeList,
+		// UserInfo,
+		// CourseGoals,
+		ActiveGoals,
+		ManageGoals,
 	},
 
 	data() {
 		return {
+			selectedComponent: "active-goals",
 			activeUser: {
 				name: "Maximilian Schwarzmüller",
 				description: "Site owner and admin",
 				role: "admin",
 			},
 		};
+	},
+
+	methods: {
+		setSelectedComponent(cmp) {
+			this.selectedComponent = cmp;
+		},
 	},
 };
 </script>
@@ -56,3 +69,21 @@ body {
 	margin: 0;
 }
 </style>
+
+<!-- 
+// move to the template section under header
+// <badge-list></badge-list>
+// <user-info
+// 	:full-name="activeUser.name"
+// 	:info-text="activeUser.description"
+// 	:role="activeUser.role"
+// ></user-info>
+// <course-goals>
+// 	<template #default="slotProps">
+// 		<h2>{{ slotProps.item }}</h2>
+// 		<p>
+// 			{{ slotProps["another-prop"] }}
+// 		</p>
+// 	</template>
+// </course-goals> 
+-->
