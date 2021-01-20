54a65e58126da3ac172d8378a9085778d2ee1386
Jude Harding <jharding@genesisdla.com>
2021-01-20 18:09:19 -0500

    finised sect 09

--- sect-09-cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ sect-09-cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -4,12 +4,15 @@
 		<!-- dummy field  -->
 		<input type="text" ref="goal" />
 		<button v-on:click="setGoal">Set Goal</button>
-		<!-- slot from erroralert  -->
-		<error-alert v-if="inputIsInvalid">
-			<h2>Input is Invalid!</h2>
-			<p>Please enter a few characters</p>
-			<button v-on:click="confirmError">Okay</button>
-		</error-alert>
+
+		<teleport to="body">
+			<!-- slot from erroralert  -->
+			<error-alert v-if="inputIsInvalid">
+				<h2>Input is Invalid!</h2>
+				<p>Please enter a few characters</p>
+				<button v-on:click="confirmError">Okay</button>
+			</error-alert>
+		</teleport>
 	</div>
 </template>
 
