735b879857eeb0711425554425421872455c6852
Jude Harding <jharding@genesisdla.com>
2021-01-20 17:50:24 -0500

    working thru vid 116

--- sect-09-cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ sect-09-cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -2,6 +2,44 @@
 	<div>
 		<h2>Manage Goals</h2>
 		<!-- dummy field  -->
-		<input type="text" />
+		<input type="text" ref="goal" />
+		<button v-on:click="setGoal">Set Goal</button>
+		<!-- slot from erroralert  -->
+		<error-alert v-if="inputIsInvalid">
+			<h2>Input is Invalid!</h2>
+			<p>Please enter a few characters</p>
+			<button v-on:click="confirmError">Okay</button>
+		</error-alert>
 	</div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+	components: {
+		ErrorAlert,
+	},
+
+	data() {
+		return {
+			inputIsInvalid: false,
+		};
+	},
+
+	methods: {
+		setGoal() {
+			const enteredValue = this.$refs.goal.value;
+
+			if (enteredValue === "") {
+				// alert("Input must not be empty");
+				this.inputIsInvalid = true;
+			}
+		},
+
+		confirmError() {
+			this.inputIsInvalid = false;
+		},
+	},
+};
+</script>
