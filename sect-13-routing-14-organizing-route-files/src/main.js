1c8e1a19352e8bebcb08c94018777941997eeda9
Jude Harding <jharding@genesisdla.com>
2021-01-24 17:53:21 -0500

    finihed sect 13

--- sect-13-routing-14-organizing-route-files/src/main.js
+++ sect-13-routing-14-organizing-route-files/src/main.js
@@ -3,6 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 
+// put any NAVIGATION GUARDS ABOVE THE CREATEAPP
+
+// router.beforeEach(to, from, next){
+//   console.log(to, from, next);
+//   // can add AUTHENTICATIOIN ON EVERY PAGE HERE
+//   next();
+// };
+
 const app = createApp(App);
 
 app.use(router);
